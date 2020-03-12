6a1a330b429b0fc87c9fcb4dfb29db8ead29bec4
Sean Morris <sean@seanmorr.is>
2020-03-11 21:58:41 -0400

    Bin cmd

--- bin/pobot.js
+++ bin/pobot.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 const [bin, script, ...args] = process.argv;
 
-const Pobot = require('./Pobot');
+const Pobot = require('../Pobot');
 
 Pobot.get(args).then(pobot => pobot.run(args)).then(()=>{
 
-	console.log('Done with chrome');
+	console.log('Done.');
 
 });
